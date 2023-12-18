711d281e2f9719db80aafea98c2f614aec1b970e
Soulz <19002275@alumnos.utleon.edu.mx>
2023-12-18 10:28:15 -0600

    modificaciones

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -14,34 +14,14 @@ export class auth {
     public URL = "ConfiguracionesComponent.apiUrl";
     constructor(private http: HttpClient) { }
 
-    
+
     login(datos:I_login): Observable<R_login>{
         return this.http.post<R_login>(`${this.URL}api/auth/login`,datos);
     }
     logout(id:number):Observable<I_mensaje>{
         return this.http.post<I_mensaje>(`${this.URL}api/auth/logout/${id}`,{});
     }
-  
-  
-    // Operación GET
-    // getData(): Observable<any> {
-    //   return this.http.get<any>(this.apiUrl);
-    // }
-  
-    // // Operación POST
-    // postData(data: any): Observable<any> {
-    //   return this.http.post<any>(this.apiUrl, data);
-    // }
-  
-    // // Operación PUT
-    // putData(id: number, data: any): Observable<any> {
-    //   const url = `${this.apiUrl}/${id}`;
-    //   return this.http.put<any>(url, data);
-    // }
-  
-    // // Operación DELETE
-    // deleteData(id: number): Observable<any> {
-    //   const url = `${this.apiUrl}/${id}`;
-    //   return this.http.delete<any>(url);
-    // }
+
+
+   
 }
